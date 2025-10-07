347cb706021ec64e1911e9154a393393c5b7ad26
tyransoft <tyransoftagency@gmail.com>
2025-10-07 17:38:57 +0200

    mail

--- project/project/settings.py
+++ project/project/settings.py
@@ -140,10 +140,10 @@ EMAIL_HOST = "mail.liysta.ly"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = "info@liysta.ly"
-EMAIL_HOST_PASSWORD ='7xDs352Jp[{c}]UU'
-DEFAULT_FROM_EMAIL = 'Liysta Platform <info@liysta.ly>'
-SERVER_EMAIL = 'Liysta Platform <info@liysta.ly>'
+EMAIL_HOST_USER = "liystaplatform@liysta.ly"
+EMAIL_HOST_PASSWORD ='mpdif$hwTIW7WIGU'
+DEFAULT_FROM_EMAIL = 'Liysta Platform <liystaplatform@liysta.ly>'
+SERVER_EMAIL = 'Liysta Platform <liystaplatform@liysta.ly>'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
