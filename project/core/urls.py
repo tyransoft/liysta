98aef8adf4d4bb3cc806fd51ccd40dfe28ec320c
tyransoft <tyransoftagency@gmail.com>
2025-11-13 21:15:22 +0200

    maop

--- project/core/urls.py
+++ project/core/urls.py
@@ -10,8 +10,8 @@ sitemaps={
 }
 
 urlpatterns = [
-    path('sitemap.xml/',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt/',TemplateView.as_view(template_name='robots.txt',content_type='text/plain')),
+    path('sitemap.xml',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt',TemplateView.as_view(template_name='robots.txt',content_type='text/plain')),
 
     path('',home,name='home'),
 
