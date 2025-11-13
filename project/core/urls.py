9fe1d208f36ec4f82d6d7299ec044da4d610fe96
tyransoft <tyransoftagency@gmail.com>
2025-11-13 21:07:55 +0200

    sitemap

--- project/core/urls.py
+++ project/core/urls.py
@@ -10,7 +10,7 @@ sitemaps={
 }
 
 urlpatterns = [
-    path('sitemaps.xml/',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml/',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt/',TemplateView.as_view(template_name='robots.txt',content_type='text/plain')),
 
     path('',home,name='home'),
