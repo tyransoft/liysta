3e1557a946f9993a713c4e11ba6ce34cb89c173c
tyransoft <tyransoftagency@gmail.com>
2025-11-15 08:04:50 +0200

    emil09

--- project/core/models.py
+++ project/core/models.py
@@ -447,7 +447,7 @@ class Liystanumbers(models.Model):
     month_data=models.FileField(upload_to='data/')
     created_at=models.DateField(auto_now_add=True)
     def __str__(self):
-        return self.created_at
+        return f"{self.created_at}"
 
 class Coasts(models.Model):
     KIND={
--- project/project/settings.py
+++ project/project/settings.py
@@ -138,14 +138,17 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "mail.liysta.ly"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = "liystaplatform@liysta.ly"
-EMAIL_HOST_PASSWORD ='mpdif$hwTIW7WIGU'
-DEFAULT_FROM_EMAIL = 'Liysta Platform <liystaplatform@liysta.ly>'
-SERVER_EMAIL = 'Liysta Platform <liystaplatform@liysta.ly>'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True   
+EMAIL_USE_SSL = False  
+
+EMAIL_HOST_USER = "liystacompany@gmail.com"
+
+EMAIL_HOST_PASSWORD = "jihd garj dnzj aqyo"
+
+DEFAULT_FROM_EMAIL = "Liysta Platform <liystaplatform@liysta.ly>"
+SERVER_EMAIL = "liystaplatform@liysta.ly"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
