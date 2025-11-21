0213f8b51faa922403efeaddcf52a1c6b9013698
tyransoft <tyransoftagency@gmail.com>
2025-11-21 14:45:32 +0200

    emailo

--- project/project/settings.py
+++ project/project/settings.py
@@ -137,14 +137,14 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "mail.liysta.ly"
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False   
-EMAIL_USE_SSL = True  
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
 
 EMAIL_HOST_USER = "liystacompany@gmail.com"
 
-EMAIL_HOST_PASSWORD = 'bcyp vaar frgd ejlw'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_APP")
 
 DEFAULT_FROM_EMAIL = "Liysta Platform <liystacompany@gmail.com"
 SERVER_EMAIL = "liystacompany@gmail.com"
