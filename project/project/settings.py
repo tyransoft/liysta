0735864a0826c9093de8a70bed40676dcf512f27
tyransoft <tyransoftagency@gmail.com>
2025-12-21 15:47:53 +0200

    uni

--- project/project/settings.py
+++ project/project/settings.py
@@ -106,6 +106,8 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset':'utf8mb4',
+            'use_unicode':True
         }
     }
 } 
