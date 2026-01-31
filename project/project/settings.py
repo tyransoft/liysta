924151eae1c7d7dfcb84e1a251c4c18baef54676
tyransoft <tyransoftagency@gmail.com>
2026-01-31 10:41:17 +0200

    ghyt

--- project/project/settings.py
+++ project/project/settings.py
@@ -19,6 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
+
+
+
+
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
